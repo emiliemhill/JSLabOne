97c844590a7e9b178a45a65cce6696a58dbf31d2
Emilie Hill <emiliemhill@gmail.com>
2018-01-26 16:39:11 -0500

    with functions

--- script.js
+++ script.js
@@ -1,30 +1,38 @@
-var gameStart = prompt("Would you like to play with two players?");
-if (gameStart === "Yes") {
-  var name = prompt("What is your name?")
-}
-
 var userHealth = 40
 var grantHealth = 10
 var grantsLives = 3
-var userWins = 0
 var grantWins = 0
 
-while (userHealth > 0 && grantHealth > 0 && grantsLives > 0) {
-    var grantDamage = Math.floor(Math.random()*(2-1+1)+1);
-    var userDamage = Math.floor(Math.random()*(2-1+1)+1);
-    userHealth -= userDamage;
-    grantHealth -= grantDamage;
-    console.log(name + " has  " + userHealth + " health left.");
-    console.log("Grant has " + grantHealth + " health left.");
-    if (grantHealth < 1) {
-        grantsLives--;
-        userWins++;
-        console.log("Grant has " + grantsLives + " lives left.");
-        grantHealth = 10;
+function startGame() {
+    if (prompt("Would you like to play against Grant the Mighty?") === "Yes") {
+        user = prompt("What's your name?");
+        startCombat();
+    }
 }
+function startCombat() {
+    while (userHealth > 0 && grantHealth > 0 && grantsLives > 0) {
+        if (prompt("Attack or quit?") === "attack") {
+            userHealth -= getDamage()
+            grantHealth -= getDamage() 
+            console.log(user + " has  " + userHealth + " health left.");
+            console.log("Grant has " + grantHealth + " health left.");
+            if (grantHealth < 1) {
+                grantsLives--;
+                console.log("Grant has " + grantsLives + " lives left.");
+                grantHealth = 10;
+            }
+        } else {
+            break;
+        }
+        if (grantsLives === 0) {
+            console.log(user + " is the winner!");
+        } else if (userHealth === 0){
+            console.log("Grant is the winner!");
+        }
+    }
 }
-if (grantsLives === 0) {
-    console.log("You are the winner!");
-} else if (userHealth === 0){
-    console.log("Grant is the winner!");
+function getDamage() {
+    return userDamage = Math.floor(Math.random()*5)+1;
+    
 }
+startGame();
